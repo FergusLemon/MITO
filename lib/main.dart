9ddf15b31feb21ccff35493c5f4395d6ac0c4c24
Fergus Lemon <fergus.lemon@gmail.com>
2019-04-12 16:58:33 +0100

    add basic homepage

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MITO',
+      theme: ThemeData(
+          primarySwatch: Colors.red,
+      ), // ThemeData
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to MITO'),
+        ),// AppBar
+        body: Center(
+          child: Text('Hello World'),
+        ),// Center
+      ),// Scaffold
+    );// Material App
+  }
+}
