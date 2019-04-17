34528caa6254bb86170964ed6ffea974869bea03
Fergus Lemon <fergus.lemon@gmail.com>
2019-04-17 12:32:34 +0100

    Why:
    Update method name after file reorg
    
    This change addresses the need by:
    
    Update MyApp() to MitoRootWidget() and import file.

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,10 +1,11 @@
 import 'package:mito/main.dart';
+import 'package:mito/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets("Renders content", (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MitoRootWidget());
 
     expect(find.text('Welcome to MITO'), findsOneWidget);
     expect(find.text('Help Those Around You'), findsOneWidget);
