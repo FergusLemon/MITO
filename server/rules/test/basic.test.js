ff38cdfb6b331b9cbf08262b29044386d20473bf
Fergus Lemon <fergus.lemon@gmail.com>
2019-06-11 13:50:33 +0100

    Update Firestore rules and add tests
    Why:
    
    To test that a user can only perform CRUD actions on their own user
    document.
    
    This change addresses the need by:
    
    Writitng tests for CRUD actions on the users collection, implementing
    the rules and updating firebase.json for the correct path to the rules.

--- server/rules/test/basic.test.js
+++ server/rules/test/basic.test.js
@@ -4,21 +4,45 @@ const { assertFails, assertSucceeds } = require('@firebase/testing')
 describe('Database rules', () => {
   let db
   let ref
-
-  beforeAll(async () => {
-    db = await setup()
-    ref = db.collection('test-collection')
-  })
+  const mockUser = {
+    uid: '1',
+  }
+  const mockData = {
+    'users/1': {
+      firstName: 'Test',
+    },
+  }
 
   afterAll(async () => {
     await teardown()
   })
 
   test('fail when reading an unauthorized collection', async () => {
+    db = await setup()
+    ref = db.collection('test-collection')
     await expect(ref.get()).toDeny()
   })
 
-  test('fail when writing to an unauthorized collection', async () => {
-    await expect(ref.add({ username: 'maliciousUser' })).toDeny()
+  describe('Rules for a new user accessing the users collection', () => {
+    test('pass when an authorized user creates their document', async () => {
+      db = await setup(mockUser)
+      ref = db.collection('users').doc(mockUser.uid)
+      await expect(ref.set({ firstName: 'Test' })).toAllow()
+    })
+  })
+
+  describe('Rules for an existing user accessing the users collection', () => {
+    beforeEach(async () => {
+      db = await setup(mockUser, mockData)
+      ref = db.collection('users').doc(mockUser.uid)
+    })
+
+    test('pass when an authorized user reads their own document', async () => {
+      await expect(ref.get()).toAllow()
+    })
+
+    test('pass when an authorized user udpates their own document', async () => {
+      await expect(ref.update({ firstName: 'newName' })).toAllow()
+    })
   })
 })
